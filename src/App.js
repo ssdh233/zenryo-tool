1463cc2a1f9fc51124878db813f1300854a06632
ssdh233 <songsongdahu233@gmail.com>
2024-11-18 02:02:40 +0900

    Temp

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Edit from "./Edit";
 
 function App() {
+  const urlParams = new URLSearchParams(window.location.search);
+  const isEdit = urlParams.get("edit") === "1";
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {isEdit ? (
+        <Edit />
+      ) : (
+        <div>
+          Not in edit mode.{" "}
+          <button
+            onClick={() => {
+              urlParams.set("edit", "1");
+              window.location.search = urlParams.toString();
+            }}
+          >
+            Go to Edit
+          </button>
+        </div>
+      )}
     </div>
   );
 }
---
+++ src/Edit.js
@@ -0,0 +1,14 @@
+function Edit() {
+  return (
+    <div className="w-3/4 mx-auto">
+      <img
+        className="w-full"
+        src="https://pbs.twimg.com/media/FH2n81tacAY4oxl?format=jpg&name=large"
+        // src="https://pbs.twimg.com/media/GQmM9GtakAAC8lm?format=jpg&name=large"
+        alt="target"
+      />
+    </div>
+  );
+}
+
+export default Edit;
