59419a88b87a5c81c6ff4b8ff60a628611882801
ssdh233 <songsongdahu233@gmail.com>
2024-11-24 15:10:09 +0900

    Temp

--- src/Edit.tsx
+++ src/Edit.tsx
@@ -1,5 +1,22 @@
 import { Button } from "./components/ui/button";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useReducer, useRef, useState } from "react";
+import { Minus, Plus } from "lucide-react";
+
+type ScaleAction = "+0.1" | "+1" | "-0.1" | "-1" | number;
+function scaleReducer(x: number, actionOrValue: ScaleAction) {
+  switch (actionOrValue) {
+    case "+1":
+      return x + 1;
+    case "+0.1":
+      return x + 0.1;
+    case "-1":
+      return x > 1.01 ? x - 1 : x;
+    case "-0.1":
+      return x > 0.11 ? x - 0.1 : x;
+    default:
+      return actionOrValue;
+  }
+}
 
 function Edit() {
   // START, DOT1, DOT2
@@ -12,9 +29,8 @@ function Edit() {
   const [blocks, setBlocks] = useState([]);
   const divEl = useRef<HTMLDivElement>(null);
 
-  const [scale, setScale] = useState(1);
+  const [scale, setScale] = useReducer(scaleReducer, 1);
 
-  const [isAltPressing, setIsALtPressing] = useState(false);
   useEffect(() => {
     window.addEventListener("resize", () => {
       console.log(divEl.current?.getBoundingClientRect().width);
@@ -24,11 +40,15 @@ function Edit() {
   useEffect(() => {
     const keyBoardEvent = (event: KeyboardEvent) => {
       if (event.key === ".") {
-        setScale((x) => x + 0.1);
+        setScale("+0.1");
       }
 
       if (event.key === ",") {
-        setScale((x) => x - 0.1);
+        setScale("-0.1");
+      }
+
+      if (event.key === "0" && event.altKey) {
+        setScale(1);
       }
     };
     window.addEventListener("keydown", keyBoardEvent);
@@ -37,105 +57,90 @@ function Edit() {
   }, []);
 
   useEffect(() => {
-    const keyDown = (event: KeyboardEvent) => {
-      if (event.key === "Alt") {
-        event.preventDefault();
-        setIsALtPressing(true);
-      }
-    };
-
-    const keyUp = (event: KeyboardEvent) => {
-      if (event.key === "Alt") {
-        event.preventDefault();
-        setIsALtPressing(false);
-      }
-    };
-    window.addEventListener("keydown", keyDown);
-    window.addEventListener("keyup", keyUp);
-
-    return () => {
-      window.removeEventListener("keydown", keyDown);
-      window.removeEventListener("keyup", keyUp);
-    };
-  }, []);
-
-  useEffect(() => {
     const mouseWheelEvent = (event: WheelEvent) => {
-      if (isAltPressing) {
-        event.preventDefault();
+      event.preventDefault();
+
+      if (event.altKey) {
         if (event.deltaY > 0) {
-          setScale((x) => x - 0.1);
+          if (event.ctrlKey) {
+            setScale("-1");
+          } else {
+            setScale("-0.1");
+          }
         }
 
         if (event.deltaY < 0) {
-          setScale((x) => x + 0.1);
+          if (event.ctrlKey) {
+            setScale("+1");
+          } else {
+            setScale("+0.1");
+          }
         }
       }
     };
     window.addEventListener("wheel", mouseWheelEvent, { passive: false });
 
     return () => window.removeEventListener("wheel", mouseWheelEvent);
-  }, [isAltPressing]);
+  }, []);
 
-  console.log({ state, dot1 });
-  // TODO canvas or DOM?
+  const layoutRef = useRef<HTMLDivElement>(null);
+
   return (
     <>
-      <div className="flex items-center">
-        <Button
-          variant="outline"
-          size="icon"
-          onClick={() => setScale((x) => x - 1)}
-        >
-          -
+      <div className="flex items-center border-b-2 p-1">
+        <Button variant="ghost" size="icon" onClick={() => setScale("-1")}>
+          <Minus />
         </Button>
-        <Button
-          variant="outline"
-          size="icon"
-          onClick={() => setScale((x) => x - 0.1)}
-        >
-          -
+        <Button variant="ghost" size="icon" onClick={() => setScale("-0.1")}>
+          <Minus />
         </Button>
-        {Math.round(Math.floor(scale * 1000) / 10)}%
-        <Button
-          variant="outline"
-          size="icon"
-          onClick={() => setScale((x) => x + 0.1)}
-        >
-          +
+        <span className="text-sm">
+          {Math.round(Math.floor(scale * 1000) / 10)}%
+        </span>
+        <Button variant="ghost" size="icon" onClick={() => setScale("+0.1")}>
+          <Plus />
         </Button>
-        <Button
-          variant="outline"
-          size="icon"
-          onClick={() => setScale((x) => x + 1)}
-        >
-          +
+        <Button variant="ghost" size="icon" onClick={() => setScale("+1")}>
+          <Plus />
         </Button>
       </div>
       <div
-        className=""
+        className="relative"
+        ref={layoutRef}
+        style={{
+          transformOrigin: "top left",
+          transform: `scale(${scale})`,
+        }}
         onClick={(event) => {
-          if (state === "START") {
+          if (layoutRef.current) {
+            const { x, y } = layoutRef.current.getBoundingClientRect();
             setDot1({
-              x: event.clientX,
-              y: event.clientY,
+              x: (event.clientX - x) / scale,
+              y: (event.clientY - y) / scale,
             });
             setState("DOT1");
           }
-          console.log("TODO get x and y axis");
         }}
-        ref={divEl}
       >
         <img
           src="https://pbs.twimg.com/media/FH2n81tacAY4oxl?format=jpg&name=large"
           // src="https://pbs.twimg.com/media/GQmM9GtakAAC8lm?format=jpg&name=large"
           alt="target"
-          style={{
-            width: `${Math.round(Math.floor(scale * 1000) / 10)}%`,
-            maxWidth: `${Math.round(Math.floor(scale * 1000) / 10)}%`,
-          }}
+          style={
+            {
+              // width: `${Math.round(Math.floor(scale * 1000) / 10)}%`,
+              // maxWidth: `${Math.round(Math.floor(scale * 1000) / 10)}%`,
+            }
+          }
         />
-        {dot1 && <div></div>}
+        {dot1 && (
+          <div
+            className="absolute z-10 border-2"
+            style={{ top: dot1.y, left: dot1.x }}
+          >
+            dot1
+          </div>
+        )}
       </div>
     </>
   );
